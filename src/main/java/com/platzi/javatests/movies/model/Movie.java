c15f7bf602de55e0601bbea5e3413e96b207c3f7
Miguel Angel Muñoz Pozos <mmunozpozos@gmail.com>
2025-02-27 00:35:37 -0600

    App de Películas: Test de Negocio

---
+++ src/main/java/com/platzi/javatests/movies/model/Movie.java
@@ -0,0 +1,35 @@
+package com.platzi.javatests.movies.model;
+
+public class Movie {
+    private final Integer id;
+    private final String name;
+    private final int minutes;
+    private final Genre genre;
+
+    public Movie(String name, int minutes, Genre genre) {
+        this(null, name, minutes, genre);
+    }
+
+    public Movie(Integer id, String name, int minutes, Genre genre) {
+        this.id = id;
+        this.name = name;
+        this.minutes = minutes;
+        this.genre = genre;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getMinutes() {
+        return minutes;
+    }
+
+    public Genre getGenre() {
+        return genre;
+    }
+}
